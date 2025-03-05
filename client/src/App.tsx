78deb520d0fb9f240948f3c1b08d19a5b099e124
Sujoy Dutta <duttasujoy1415@gamil.com>
2025-03-05 14:32:00 +0530

    first commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,20 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 
+const basePath = "/portfolio";
+
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base={basePath}>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
